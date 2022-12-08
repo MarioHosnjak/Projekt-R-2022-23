a967a84274b97053fd253b67113b40738b15d1e8
Mario Hosnjak <mario.hosnjak009@gmail.com>
2022-12-08 22:49:20 +0100

    Stvoreni frontend i backend

---
+++ backend/src/main/java/projektr/ProjektRApplication.java
@@ -0,0 +1,14 @@
+package projektr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class}) // dodano jer trenutno jos nemamo bazu podataka
+public class ProjektRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektRApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/projektr/ProjektRApplicationTests.java
@@ -0,0 +1,13 @@
+package projektr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
