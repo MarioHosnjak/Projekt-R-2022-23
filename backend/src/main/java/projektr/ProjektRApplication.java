5241139b0698a3abe19ec651ea1a477ca60acc8e
Mario Hosnjak <mario.hosnjak009@gmail.com>
2022-12-27 15:07:37 +0100

    Dodana povezanost s lokalnom bazom podataka

--- backend/src/main/java/projektr/ProjektRApplication.java
+++ backend/src/main/java/projektr/ProjektRApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class}) // dodano jer trenutno jos nemamo bazu podataka
+//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class}) // dodano jer trenutno jos nemamo bazu podataka
+@SpringBootApplication
 public class ProjektRApplication {
 
 	public static void main(String[] args) {
