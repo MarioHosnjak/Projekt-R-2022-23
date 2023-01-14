ec8bfb8c3926f075cf13e476f192fc841ebc9f24
andrejcirkveni <andrej.cirkveni@fer.hr>
2023-01-14 19:58:18 +0100

    graf za svako mjerenje senzora

--- frontend/src/container/senzorPage/senzorPage.js
+++ frontend/src/container/senzorPage/senzorPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import "./senzorPage.css"
-import { latestOfId } from "../../utils/axios/backendcalls/senzorEndPoints";
+import { latestOfId, allOfId } from "../../utils/axios/backendcalls/senzorEndPoints";
 import {Line} from 'react-chartjs-2';
 import {testData} from './testChartData'
 import { Chart as ChartJS } from "chart.js/auto"; //NE BRISAT, veli da se nigdje ne koristi, ali bez tog ne radi graf
@@ -9,6 +9,7 @@ import { Chart as ChartJS } from "chart.js/auto"; //NE BRISAT, veli da se nigdje
 const SenzorPage = () => {
     var [idSenzora, setIdSenzora] = useState("Default");
     const [latestMeasurement, setLatestMeasurement] = useState();
+    const [allMeasurements, setAllMeasurements] = useState();
 
     const [testChartData,setTestData] = useState({
         labels: testData.map((data) => data.time),
@@ -32,24 +33,57 @@ const SenzorPage = () => {
             }).catch((err) => {
                 console.log(err);
             });
+            allOfId(idSenzora).then((res) => {
+                console.log(res);
+                setAllMeasurements(res);
+            }).catch((err) => {
+                console.log(err);
+            });
         }
+
     }, []);
+
+    let measurementData=[];
+    if(latestMeasurement && allMeasurements){
+        for(var key in latestMeasurement){
+            if(!["id","location","sensorId","time"].includes(key)){
+                measurementData.push(
+                <div key={key}>
+                    <h2>Latest {key}: {latestMeasurement[key]}</h2>
+                    <div style={{width:700}}>
+                            <Line 
+                                data={{
+                                    labels: allMeasurements.map((data) => data.time),
+                                    datasets: [{
+                                        label: key,
+                                        data: allMeasurements.map((data) => data[key]),
+                                        borderColor: "black",
+                                        borderWidth: 2,
+                                    }]
+                                }}
+                            />
+                        </div>
+                </div>
+                
+                );
+            }
+            
+        }
+        
+    }
 
     let measurementInfo
     if(latestMeasurement){
+        
         measurementInfo = <div>
                             <h2>Time: {latestMeasurement["time"]}</h2>
                             <h2>Location: {latestMeasurement["location"]}</h2>
                             <p>-----------------------------</p>
-                            <h2>Temperature: {latestMeasurement["temperature"]}</h2>
-                            <h2>Humidity: {latestMeasurement["humidity"]}</h2>
-                            <h2>Pressure: {latestMeasurement["pressure"]}</h2>
-                            <h2>Illuminance: {latestMeasurement["illuminance"]}</h2>
-                            <h2>UVA: {latestMeasurement["uva"]}</h2>
-                            <h2>UVB: {latestMeasurement["uvb"]}</h2>
-                            <h2>UV-Index: {latestMeasurement["uvindex"]}</h2>
+                            {measurementData}
                             </div>
     }
+    
+
 
     return (
         <div className="">
