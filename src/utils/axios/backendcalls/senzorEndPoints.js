3fd8211c627082beff94021e8ba7f45c313d92a1
bartol00 <95582435+bartol00@users.noreply.github.com>
2023-01-22 16:27:43 +0100

    modify backendcalls

--- src/utils/axios/backendcalls/senzorEndPoints.js
+++ src/utils/axios/backendcalls/senzorEndPoints.js
@@ -1,14 +1,14 @@
 import axios from "../apiClient";
 
 export const getAllSensors = async () => {
-    const response = await axios.get("http://localhost:80/sensordata/ids");
+    const response = await axios.get("http://93.137.174.47/sensordata/ids");
     return response.data;
 }
 export const latestOfId = async (id) => {
-    const response = await axios.post(`http://localhost:80/sensordata/findlatest`,{"sensorId":id});
+    const response = await axios.post(`http://93.137.174.47/sensordata/findlatest`,{"sensorId":id});
     return response.data;
 }
 export const allOfId = async (id) => {
-    const response = await axios.post(`http://localhost:80/sensordata/all`,{"sensorId":id});
+    const response = await axios.post(`http://93.137.174.47/sensordata/all`,{"sensorId":id});
     return response.data;
 }
