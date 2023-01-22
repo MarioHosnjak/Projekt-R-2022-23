d23e26bb7f8986d32fe52f32780b896317fe10f7
bartol00 <95582435+bartol00@users.noreply.github.com>
2023-01-22 15:16:49 +0100

    modify localhost port number

--- src/utils/axios/backendcalls/senzorEndPoints.js
+++ src/utils/axios/backendcalls/senzorEndPoints.js
@@ -1,14 +1,14 @@
 import axios from "../apiClient";
 
 export const getAllSensors = async () => {
-    const response = await axios.get("http://localhost:5000/sensordata/ids");
+    const response = await axios.get("http://localhost:80/sensordata/ids");
     return response.data;
 }
 export const latestOfId = async (id) => {
-    const response = await axios.post(`http://localhost:5000/sensordata/findlatest`,{"sensorId":id});
+    const response = await axios.post(`http://localhost:80/sensordata/findlatest`,{"sensorId":id});
     return response.data;
 }
 export const allOfId = async (id) => {
-    const response = await axios.post(`http://localhost:5000/sensordata/all`,{"sensorId":id});
+    const response = await axios.post(`http://localhost:80/sensordata/all`,{"sensorId":id});
     return response.data;
 }
