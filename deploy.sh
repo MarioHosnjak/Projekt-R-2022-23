7af1f80391ada2eedafce1924cfeea6262a6a9bf
bartol00 <95582435+bartol00@users.noreply.github.com>
2023-01-22 14:37:57 +0100

    chnaged localhost port to 5000

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ echo "Building app..."
 npm run build 
 
 echo "Deploying files..."
-scp -r build/* user@192.168.1.25:/var/www/93.136.114.142/
+scp -r build/* user@93.138.56.84:/var/www/93.136.114.142/
 
 echo "Done!"
--- src/utils/axios/backendcalls/senzorEndPoints.js
+++ src/utils/axios/backendcalls/senzorEndPoints.js
@@ -1,14 +1,14 @@
 import axios from "../apiClient";
 
 export const getAllSensors = async () => {
-    const response = await axios.get("http://localhost:8080/sensordata/ids");
+    const response = await axios.get("http://localhost:5000/sensordata/ids");
     return response.data;
 }
 export const latestOfId = async (id) => {
-    const response = await axios.post(`http://localhost:8080/sensordata/findlatest`,{"sensorId":id});
+    const response = await axios.post(`http://localhost:5000/sensordata/findlatest`,{"sensorId":id});
     return response.data;
 }
 export const allOfId = async (id) => {
-    const response = await axios.post(`http://localhost:8080/sensordata/all`,{"sensorId":id});
+    const response = await axios.post(`http://localhost:5000/sensordata/all`,{"sensorId":id});
     return response.data;
 }
