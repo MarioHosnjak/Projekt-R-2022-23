4437e33a839c4e4cc05757df3ea24e96cf15cfd7
bartol00 <95582435+bartol00@users.noreply.github.com>
2023-01-23 01:06:35 +0100

    dodan url za dynamic ip

--- src/utils/axios/backendcalls/senzorEndPoints.js
+++ src/utils/axios/backendcalls/senzorEndPoints.js
@@ -1,14 +1,14 @@
 import axios from "../apiClient";
 
 export const getAllSensors = async () => {
-    const response = await axios.get("http://93.137.174.47/sensordata/ids");
+    const response = await axios.get("http://projektr.hopto.org/sensordata/ids");
     return response.data;
 }
 export const latestOfId = async (id) => {
-    const response = await axios.post(`http://93.137.174.47/sensordata/findlatest`,{"sensorId":id});
+    const response = await axios.post(`http://projektr.hopto.org/sensordata/findlatest`,{"sensorId":id});
     return response.data;
 }
 export const allOfId = async (id) => {
-    const response = await axios.post(`http://93.137.174.47/sensordata/all`,{"sensorId":id});
+    const response = await axios.post(`http://projektr.hopto.org/sensordata/all`,{"sensorId":id});
     return response.data;
 }
